4620045a17073cd9ea6f6a417ce0c3a2c9cb629e
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2022-12-26 00:24:34 -0300

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+let api_key = "b5fef8eb083d23a7b63208a1704fdf94";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
